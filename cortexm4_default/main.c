5264987516a3e37e51c1194999108a074795ca17
panxinhao <xinhao.pan@pimchip.cn>
2024-04-09 09:10:57 +0800

    ✨ feat(cm4): led+uart test

--- cortexm4_default/main.c
+++ cortexm4_default/main.c
@@ -2,7 +2,7 @@
  * @Author       : panxinhao
  * @Date         : 2023-07-25 11:04:26
  * @LastEditors  : panxinhao
- * @LastEditTime : 2024-03-28 15:25:29
+ * @LastEditTime : 2024-04-09 08:42:24
  * @FilePath     : \\ne004-plus\\cortexm4_default\\main.c
  * @Description  :
  *
@@ -29,33 +29,34 @@ int main(void)
 {
     uint32_t temp = 0x0U;
     /* disable all interrupt */
-    // REG32(0x4000D0F8U) = 0x0U;  //屏蔽riscv所有外部中断
-    // REG32(0x4000D0FCU) = 0x0U;  //屏蔽riscv2arm所有中断
-    // REG64(0x4000D174U) = 0x0U;  //屏蔽arm所有个中断
-    // REG64(0x4000D17CU) = 0x0U;  //屏蔽arm2riscv所有中断
+    REG32(0x4000D0F8U) = 0x0U;  //屏蔽riscv所有外部中断
+    REG32(0x4000D0FCU) = 0x0U;  //屏蔽riscv2arm所有中断
+    REG64(0x4000D174U) = 0x0U;  //屏蔽arm所有个中断
+    REG64(0x4000D17CU) = 0x0U;  //屏蔽arm2riscv所有中断
     /* config IO */
-    // PAD_GP7_FUNCSEL  |= (0x3 << 28); // JTAG
-    // PAD_GP8_FUNCSEL  |= (0x3);
-    // PAD_GP9_FUNCSEL  |= (0x3 << 4);
-    // PAD_GP10_FUNCSEL |= (0x03 << 8);
-    // PAD_GP13_FUNCSEL |= (0x01 << 20); // riscv UART
-    // PAD_GP14_FUNCSEL |= (0x1 << 24);
-    // PAD_GP15_FUNCSEL |= (0x02 << 28); // ARM UART
-    // PAD_GP16_FUNCSEL |= (0x2);
-    // UART_BAUD(UART0) = UART_BAUD_DIV & (12500000 / 115200);
-    // UART_CTRL(UART0) = UART_CTRL_TEN | UART_CTRL_REN;
     REG32(0x4000D140) = 0x1;
     REG32(0x4000D144) = 0x0;
-    REG32(0x4000D148) = 0x110;
+    REG32(0x4000D148) = 0x0;
     REG32(0x40006004) = 0xFFFFFFFF;
+    PAD_GP7_FUNCSEL  |= (0x3 << 28); // JTAG
+    PAD_GP8_FUNCSEL  |= (0x3);
+    PAD_GP9_FUNCSEL  |= (0x3 << 4);
+    PAD_GP10_FUNCSEL |= (0x03 << 8);
+    PAD_GP13_FUNCSEL |= (0x01 << 20); // riscv UART
+    PAD_GP14_FUNCSEL |= (0x1 << 24);
+    PAD_GP15_FUNCSEL |= (0x02 << 28); // ARM UART
+    PAD_GP16_FUNCSEL |= (0x2);
+    UART_BAUD(UART0) = UART_BAUD_DIV & (100000000 / 115200);
+    UART_CTRL(UART0) = UART_CTRL_TEN | UART_CTRL_REN;
     while (1)
     {
         /* code */
-        // UART_DATA(UART0) = 0x5A;
+        UART_DATA(UART0) = 0x5A;
         REG32(0x40006000) = 0xFFFFFFFF;
-        arm_delay_us(10);
+        arm_delay_us(1000000);
         REG32(0x40006000) = 0x0;
-        arm_delay_us(10);
+        UART_DATA(UART0) = 0xA5;
+        arm_delay_us(1000000);
     }
     (void)temp;
     return 0;

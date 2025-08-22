f7391065b8e60bda0f431da6ff9a5db858111ffc
xingxinonline <xingxinonline@126.com>
2025-08-22 16:43:05 +0800

    fix(build): 避免与 system_S300.c 重复定义 SysTick，通用源仅保留 retarget.c

--- S300_BSP/tools/bsp_common.mk
+++ S300_BSP/tools/bsp_common.mk
@@ -6,9 +6,8 @@
 CFLAGS += -I$(ROOT)/CMSIS/Device/PiMCHIP/S300/Include -I$(ROOT)/CMSIS/Core/Include \
           -I$(ROOT)/Drivers/BSP/Include -I$(ROOT)/Drivers/UART/Include -I$(ROOT)/Boards/S300_EVB -I$(ROOT)/Drivers/SoC/Include
 
-# Pull in retarget.c/systick.c by default
-BSP_COMMON_SRCS ?= $(ROOT)/Drivers/BSP/Source/retarget.c \
-				   $(ROOT)/Drivers/BSP/Source/systick.c
+# Pull in retarget.c by default
+BSP_COMMON_SRCS ?= $(ROOT)/Drivers/BSP/Source/retarget.c
 
 # Link flags to enable float and long long formatting in newlib-nano
 LDFLAGS += -u _printf_float -u _scanf_float -u _printf_long_long -u _scanf_long_long

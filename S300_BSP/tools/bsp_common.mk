b0f3c7aaf78e755f547982cdad43b067d9cc7e1f
xingxinonline <xingxinonline@126.com>
2025-08-22 16:41:53 +0800

    chore(bsp): 清理重复/空的 RCC 源文件并完善通用构建
    - 删除 Drivers/BSP/Source/s300_rcc.c 与 Drivers/SoC/Source/s300_rcc.c 的空实现
    - 统一使用 SoC 层 s300_rcc.h 中的 inline RCC 控制函数
    - 在 bsp_common.mk 中纳入 systick.c，保持通用组件可编译

--- S300_BSP/tools/bsp_common.mk
+++ S300_BSP/tools/bsp_common.mk
@@ -6,8 +6,9 @@
 CFLAGS += -I$(ROOT)/CMSIS/Device/PiMCHIP/S300/Include -I$(ROOT)/CMSIS/Core/Include \
           -I$(ROOT)/Drivers/BSP/Include -I$(ROOT)/Drivers/UART/Include -I$(ROOT)/Boards/S300_EVB -I$(ROOT)/Drivers/SoC/Include
 
-# Pull in retarget.c by default
-BSP_COMMON_SRCS ?= $(ROOT)/Drivers/BSP/Source/retarget.c
+# Pull in retarget.c/systick.c by default
+BSP_COMMON_SRCS ?= $(ROOT)/Drivers/BSP/Source/retarget.c \
+				   $(ROOT)/Drivers/BSP/Source/systick.c
 
 # Link flags to enable float and long long formatting in newlib-nano
 LDFLAGS += -u _printf_float -u _scanf_float -u _printf_long_long -u _scanf_long_long
